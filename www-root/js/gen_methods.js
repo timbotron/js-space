5df9d85c9bf156c83f7c7676c9908947fd751736
Tim Habersack <timh@darkhorse.com>
2015-12-22 13:41:24 -0800

    change

--- www-root/js/gen_methods.js
+++ www-root/js/gen_methods.js
@@ -4,19 +4,19 @@ function output_this(str) {
   output[0].innerHTML += str +'<br />';
 }
 
-function open_local_json(path) {
+function generate_galaxy() {
 	var oReq = new XMLHttpRequest();
 	function saveJSON() {
 		tmp = JSON.parse(this.responseText);
 		console.log(tmp);
 	};
 	oReq.addEventListener("load", saveJSON);
-	oReq.open("GET", path);
+	oReq.open("GET", '/js/config.mk2.json');
 	oReq.send();
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  open_local_json('/js/config.mk2.json');
+  generate_galaxy();
 
 });
 
