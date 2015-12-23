82cb8d1b1156eb0ae9b2a57f333d8b70a2a05d9b
Tim Habersack <tim@hithlonde.com>
2015-12-23 08:38:55 -0800

    changes

--- www-root/js/gen_methods.js
+++ www-root/js/gen_methods.js
@@ -1,22 +1,35 @@
-var ret;
+function s(v,val) {
+	sessionStorage.setItem(v,val);
+}
+function g(v) {
+	sessionStorage.getItem(v);
+}
 function output_this(str) {
 	var output = document.getElementsByClassName('output');
   output[0].innerHTML += str +'<br />';
 }
 
-function generate_galaxy() {
+function generate_cradle() {
+	config = JSON.parse(this.responseText);
+	console.log(config);
+	s('config',config);
+	// let us begin!
+	
+	
+}
+function test() {
+	console.log(g('config'));	
+}
+
+function start() {
 	var oReq = new XMLHttpRequest();
-	function saveJSON() {
-		tmp = JSON.parse(this.responseText);
-		console.log(tmp);
-	};
-	oReq.addEventListener("load", saveJSON);
+	oReq.addEventListener("load", generate_cradle);
 	oReq.open("GET", '/js/config.mk2.json');
 	oReq.send();
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  generate_galaxy();
+  start();
 
 });
 
